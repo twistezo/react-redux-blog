d96b94e0f34583a3a95432f07bf187a7819025d2
twistezo <kolkolukasz@gmail.com>
2019-02-21 19:34:48 +0100

    Updates

---
+++ precache-manifest.49e6f0aaae58ed0ffffec852d2ab2e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42db9016b535730b7f2b",
+    "url": "/react-redux-blog/static/js/runtime~main.42db9016.js"
+  },
+  {
+    "revision": "bac3ddb63d87df4aeeb9",
+    "url": "/react-redux-blog/static/js/main.bac3ddb6.chunk.js"
+  },
+  {
+    "revision": "832c6a405fd1a4b65d53",
+    "url": "/react-redux-blog/static/js/2.832c6a40.chunk.js"
+  },
+  {
+    "revision": "bac3ddb63d87df4aeeb9",
+    "url": "/react-redux-blog/static/css/main.04b1b254.chunk.css"
+  },
+  {
+    "revision": "d4ab3f928f2bb765debf72198f50734d",
+    "url": "/react-redux-blog/index.html"
+  }
+];
