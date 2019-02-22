e27a00b70297a03bfc31f5f54621fb9daee6ae9e
twistezo <kolkolukasz@gmail.com>
2019-02-22 21:50:23 +0000

    Updates

--- precache-manifest.3d15e462acc07d2065f0925efc9b0d56.js
+++ precache-manifest.3d15e462acc07d2065f0925efc9b0d56.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-redux-blog/static/js/runtime~main.42db9016.js"
   },
   {
-    "revision": "9b1bef7c1fb41992019c",
-    "url": "/react-redux-blog/static/js/main.9b1bef7c.chunk.js"
+    "revision": "5fb32fad9d46a9315f6c",
+    "url": "/react-redux-blog/static/js/main.5fb32fad.chunk.js"
   },
   {
     "revision": "f2580cf7d31721fbe57f",
     "url": "/react-redux-blog/static/js/2.f2580cf7.chunk.js"
   },
   {
-    "revision": "9b1bef7c1fb41992019c",
-    "url": "/react-redux-blog/static/css/main.399a6bde.chunk.css"
+    "revision": "5fb32fad9d46a9315f6c",
+    "url": "/react-redux-blog/static/css/main.8b0c88ca.chunk.css"
   },
   {
-    "revision": "b5c9e9e977a1654c1b38d5221f8ef3a3",
+    "revision": "b846418589ae6266068c9af4c7ce8671",
     "url": "/react-redux-blog/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-redux-blog/precache-manifest.656ea267fdfa4d795b2486bed5e4a304.js"
+  "/react-redux-blog/precache-manifest.3d15e462acc07d2065f0925efc9b0d56.js"
 );
 
 workbox.clientsClaim();
